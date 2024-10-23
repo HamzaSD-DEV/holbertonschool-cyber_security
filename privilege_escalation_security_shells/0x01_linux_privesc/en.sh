bb4317262bb81021319e655d20fdf6c3c5d451a3
HamzaSD-DEV <saoudh20@gmail.com>
2024-10-23 11:03:53 +0200

    PE LPE T0 1

--- privilege_escalation_security_shells/0x01_linux_privesc/en.sh
+++ privilege_escalation_security_shells/0x01_linux_privesc/en.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 gen_flag () {
-    md5sum <<< $(openssl aes-256-cbc -pass pass:$1 -nosalt -pbkdf2 <<< $2) | head -c 32
+    md5sum <<< $(openssl aes-256-cbc -pass pass:$1 -nosalt -pbkdf2 <<< HamzaSD-DEV) | head -c 32
 }
 
 
-export github_username="HamzaSD-DEV"
-export FLAG_0=$(gen_flag EBXT7S8D69GUACKK $github_username)
-
-# Create the flag file and set appropriate permissions
-echo "Holberton{root_periodic_script_escalation $FLAG_0}" 
+gen_flag
