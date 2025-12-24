697ad9bd907456fa04d1255fce44ad7de6469027
HamzaSD-DEV <saoudh20@gmail.com>
2025-12-24 12:11:14 +0100

    update task0 of Mandatory Access Control

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(command -v sestatus >/dev/null 2>&1 && sestatus | grep -i '^SELinux status:') || { m=$(getenforce 2>/dev/null | tr 'A-Z' 'a-z'); [ -n "$m" ] && echo "SELinux status:                 $m" || echo "SELinux status:                 unknown"; }
+sestatus | grep "SELinux status:" | awk '{print "SELinux status: " $3}'
